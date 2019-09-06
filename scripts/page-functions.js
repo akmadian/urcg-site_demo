5e4d1b074901a4233c3c7541b8e86429b4af10b5
Ari <akmadian@gmail.com>
2019-09-05 18:45:35 -0700

    Fix loads of CSS, prep for pulling some assets with HTTPS

--- scripts/page-functions.js
+++ scripts/page-functions.js
@@ -1,20 +1,3 @@
-$(document).ready(function(){
-    $(".facts-carousel").slick({
-        autoplay: true,
-        accessibility: true,
-        autoplaySpeed: 4000
-    });
-});
-
-$(document).ready(function(){
-    $(".testimonials-carousel").slick({
-        autoplay: true,
-        accessibility: true,
-        fade: true,
-        autoplaySpeed: 4500
-    });
-});
-
 function GASendEvent(eventLabel) {
     console.log("Sending event hit...")
     ga("send", {
